863f5d0c2571455292f9001923332fd4ef4fe5bb
Maxiwee <81492222+maxiwee69@users.noreply.github.com>
2023-12-21 15:53:00 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-if you think this is fake or something watch https://www.youtube.com/watch?v=yWqqMp6ca30&t
+# Nitrogen
 
-This script sends POST requests to a specified URL and writes the received tokens to a file named 'nitrolinks.txt'. The script continues to send requests until it has been rate limited 5 times in a row.
+A simple tool to generate lots of free nitro links using the operagx free 
+nitro campaign
 
-Modules:
+The proxy part is currently disabled because it always says rate limited after 1 request(which isnt true) so if you know how to fix this pls submitt a pull request
 
-requests: Used to send HTTP requests.
-json: Used to parse the JSON response from the server.
-Constants:
+## Installation
+```
+git clone https://github.com/maxiwee69/nitrogen.git
+```
+```
+pip install requests
+```
 
-QUANTITY: The number of times to call the gen function after the rate limit check. Currently set to 2.
-base_path: The base URL to which the received token is appended to form the complete link.
-url: The URL to which the POST requests are sent.
-payload: The JSON payload to be sent in the POST request.
-headers: The headers to be included in the POST request.
-Functions:
+# Usage
 
-gen: This function sends a POST request to the specified URL and writes the received token to a file. If the server responds with a 429 status code (rate limit exceeded), the function returns False. If the server responds with a 2xx status code (successful), the function writes the token to a file and returns True. If the server responds with any other status code, the function prints an error message and returns True.
-Main Script: The script first checks for rate limiting by calling the gen function in a while loop until it has been rate limited 5 times in a row. After the rate limit check, the gen function is called QUANTITY times.
+```
+python main.py
+```
+# Contributing
+Anyone that wants to contribute to this project is allowed to do so by simply creating a fork and submitting a pull request
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import requests
 import json
-import string
+import sys
 
-# Constants
 QUANTITY = 2
 base_path = "https://discord.com/billing/partner-promotions/1180231712274387115/"
 url = 'https://api.discord.gx.games/v1/direct-fulfillment'
 payload = {
     "partnerUserId": "d05d65629f9b076a55c0661fcf7e9871bbf7052042d26b5185784d29f06081ab"
 }
-
-# Headers for the HTTP request
 headers = {
     'authority': 'api.discord.gx.games',
     'accept': '*/*',
@@ -27,21 +24,53 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0'
 }
 
-def gen():
-    """
-    This function sends a POST request to the specified URL and writes the received token to a file.
-    If the server responds with a 429 status code (rate limit exceeded), the function returns False.
-    If the server responds with a 2xx status code (successful), the function writes the token to a file and returns True.
-    If the server responds with any other status code, the function prints an error message and returns True.
-    """
 
-    # Send a POST request
-    response = requests.post(url, json=payload, headers=headers)
+def gen():
+    # Add a variable to track whether a proxy has been used
+    # used_proxy = False
+    proxies = None
+
+    # Send a POST request without a proxy
+    response = requests.post(url, json=payload, headers=headers, proxies=proxies)
 
     # Check if the rate limit has been exceeded
     if response.status_code == 429:
-        print("Rate limited")
-        return False
+        print("You have been rate limited. Exiting the program.")
+        sys.exit()
+
+        # The following code is commented out because the proxy part is unstable
+        """
+        if used_proxy:
+            # Ask the user if they want to use another proxy
+            use_another_proxy = input("The current proxy has been rate limited. Do you want to use another proxy? (yes/no): ").lower() == "yes"
+        else:
+            print("You have been rate limited. Do you want to use a proxy?")
+            use_proxy = input("Do you want to use a proxy? (yes/no): ").lower()
+            if use_proxy == "yes":
+                proxy = input("Please enter the proxy URL: ")
+                proxy_auth_needed = input("Is authentication needed for the proxy? (yes/no): ").lower() == "yes"
+                if proxy_auth_needed:
+                    proxy_username = input("Please enter the proxy username: ")
+                    proxy_password = input("Please enter the proxy password: ")
+                    proxies = {
+                        "http": f"http://{proxy_username}:{proxy_password}@{proxy}",
+                        "https": f"https://{proxy_username}:{proxy_password}@{proxy}",
+                    }
+                else:
+                    proxies = {
+                        "http": proxy,
+                        "https": proxy,
+                    }
+                used_proxy = True  # Move this line here
+            else:
+                print("Exiting the program as no proxy is to be used.")
+                sys.exit()
+
+        # Send a POST request with a proxy
+        response = requests.post(url, json=payload, headers=headers, proxies=proxies)
+        """
+
+    # Rest of the code...
 
     # Check if the request was successful
     if response.status_code // 100 == 2:
@@ -64,3 +93,13 @@ def gen():
         print("Response text:", response.text)
 
     return True
+
+rate_limit_count = 0
+while rate_limit_count < 5:
+    if not gen():
+        rate_limit_count += 1
+    else:
+        rate_limit_count = 0
+
+for i in range(QUANTITY):
+    gen()
